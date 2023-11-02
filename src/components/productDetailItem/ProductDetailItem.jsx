4679d3648eff5861e2d0ff45b9fd1d62e8d2f3c6
prajzz7 <pandeshwarprajwal@gmail.com>
2023-11-02 15:17:53 +0530

    ProductEdit page created without styling and edit functionality

--- src/components/productDetailItem/ProductDetailItem.jsx
+++ src/components/productDetailItem/ProductDetailItem.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const ProductDetailItem = ({data}) => {
+  const {title, description, rating:{rate}, rating:{count}, image, price} = data
+  console.log(title)
   return (
     <div>
-        
+        <div>
+          <span>{title} </span>
+          <span>Rating: {rate} </span>
+          <span>Total reviews: {count}</span>
+        </div>
+        <div >
+          <img style={{height:'100px',width:'100px'}} src={image} alt=''></img>
+        </div>
+        <div>
+          Rs. {price}
+        </div>
+        <div>
+          {description}
+        </div>
     </div>
   )
 }
