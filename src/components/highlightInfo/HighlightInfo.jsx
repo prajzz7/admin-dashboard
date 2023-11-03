70075ed32ac9b777acb1afee14980a21eba1221a
pratik-G-ARKA <pgupta@arka.energy>
2023-11-03 22:39:21 +0530

    changes

--- src/components/highlightInfo/HighlightInfo.jsx
+++ src/components/highlightInfo/HighlightInfo.jsx
@@ -16,37 +16,6 @@ const HighlightInfo = () => {
 
             </div>
         </div>
-        <div className='highlightItem'>
-            <h3 className='highlightTitle'>Total Revenue</h3>
-            <span className='highlightMoney'>₹ 32,50,456</span>
-            <div className='highlightAnalyticsContainer'>
-                <span>-2.78%</span> <ArrowDownwardIcon className='arrow negative'/>
-                <span>Since last week 1</span>
-                <span>Since last week 1</span>
-
-            </div>
-        </div>
-        <div className='highlightItem'>
-            <h3 className='highlightTitle'>Users</h3>
-            <span className='highlightMoney'>50,456</span>
-            <div className='highlightAnalyticsContainer'>
-                <span> +5.74% </span> <ArrowUpwardIcon className='arrow'/>
-                <span>Since last week</span>
-            </div>
-        </div>
-        <div className='highlightItem'>
-            <h3 className='highlightTitle'>PRAJWAL PANDESHWAR ====== </h3>
-            <span className='highlightMoney'>1,25,045</span>
-            <span className='highlightMoney'>1,25,045</span>
-            <span className='highlightMoney'>1,25,045</span>
-            <span className='highlightMoney'>1,25,045</span>
-            <span className='highlightMoney'>1,25,045</span>
-            
-            <div className='highlightAnalyticsContainer'>
-                <span>+8.28%</span> <ArrowUpwardIcon className='arrow'/>
-                <span>Since last week</span>
-            </div>
-        </div>
     </div>
   )
 }
