30841705c210b53316b8611914154a894c87aaa1
pratik-G-ARKA <pgupta@arka.energy>
2023-11-03 22:03:01 +0530

    highLight-Info

--- src/components/highlightInfo/HighlightInfo.jsx
+++ src/components/highlightInfo/HighlightInfo.jsx
@@ -11,7 +11,7 @@ const HighlightInfo = () => {
             <span className='highlightMoney'>₹ 32,50,456</span>
             <div className='highlightAnalyticsContainer'>
                 <span>-2.78%</span> <ArrowDownwardIcon className='arrow negative'/>
-                <span>Since last week</span>
+                <span>Since last week - 2</span>
             </div>
         </div>
         <div className='highlightItem'>
