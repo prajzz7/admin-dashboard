79f9ec63f701c135c74af5867649cba851786cf4
prajzz7 <pandeshwarprajwal@gmail.com>
2023-11-04 14:21:25 +0530

    Just a temp commit

--- src/components/highlightInfo/HighlightInfo.jsx
+++ src/components/highlightInfo/HighlightInfo.jsx
@@ -7,19 +7,30 @@ const HighlightInfo = () => {
   return (
     <div className='highlightInfo'>
         <div className='highlightItem'>
-            <span className='highlightMoney'>₹ 32,50,456</span>
+            <h3 className='highlightTitle'>Total Revenue</h3>
             <span className='highlightMoney'>₹ 32,50,456</span>
             <div className='highlightAnalyticsContainer'>
                 <span>-2.78%</span> <ArrowDownwardIcon className='arrow negative'/>
                 <span>Since last week 1</span>
                 <span>Since last week 1</span>
-                <span>This span added</span>
-                <span>This span added</span>
-                <span>This span added</span>
+
             </div>
         </div>
-        <div>
-            MALLU DIV
+        <div className='highlightItem'>
+            <h3 className='highlightTitle'>Users</h3>
+            <span className='highlightMoney'>50,456</span>
+            <div className='highlightAnalyticsContainer'>
+                <span> +5.74% </span> <ArrowUpwardIcon className='arrow'/>
+                <span>Since last week</span>
+            </div>
+        </div>
+        <div className='highlightItem'>
+            <h3 className='highlightTitle'>Debt</h3>
+            <span className='highlightMoney'>1,25,045</span>
+            <div className='highlightAnalyticsContainer'>
+                <span>+8.28%</span> <ArrowUpwardIcon className='arrow'/>
+                <span>Since last week SOme REVENEUE</span>
+            </div>
         </div>
     </div>
   )
